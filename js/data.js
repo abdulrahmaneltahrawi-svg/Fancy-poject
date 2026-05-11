7a03bda00feb25fa6621c033383bdd7fa6d977f7
Abdulrahman <abdulrhmanelthrawi@gmail.com>
2026-05-11 14:48:04 +0300

    dynamic card and view items

---
+++ js/data.js
@@ -0,0 +1,57 @@
+// data.js
+const projectsData = [
+    {
+        id: "buffet",
+        title: "كرسي",
+        image: "imges/img/img4.jpg",
+        category: "Interior Design",
+        tag: "Restaurant",
+        description: "تصميم داخلي متكامل يعتمد على الإضاءة المخفية والخامات الطبيعية ليعطي تجربة فخمة للزوار.",
+        whatsapp: "966530559095"
+    },
+    {
+        id: "office",
+        title: "مكتب إداري فاخر",
+        image: "imges/img/img21.jfif",
+        category: "Interior Design",
+        tag: "Office",
+        description: "مساحة عمل ذكية بتصميم يجمع بين العملية والأناقة، مع استخدام قواطع زجاجية لزيادة الإضاءة.",
+        whatsapp: "966571778989"
+    },
+    {
+        id: "classic-restaurant",
+        title: "سرير نوم",
+        image: "imges/img/img7.jfif",
+        category: "Architecture",
+        tag: "Restaurant",
+        description: "دمج بين الهوية التقليدية واللمسات العصرية لتقديم تجربة بصرية فريدة للزوار.",
+        whatsapp: "966530559095"
+    },
+        {
+        id: "classic-sad",
+        title: "كرسي استرخاء",
+        image: "imges/img/img1.jpg",
+        category: "Architecture",
+        tag: "prodact",
+        description: "دمج بين الهوية التقليدية واللمسات العصرية لتقديم تجربة بصرية فريدة للزوار.",
+        whatsapp: "966530559095"
+    },
+        {
+        id: "classic-df",
+        title: "مكتب اجتماعات",
+        image: "imges/img/img3.jfif",
+        category: "Architecture",
+        tag: "prodact",
+        description: "دمج بين الهوية التقليدية واللمسات العصرية لتقديم تجربة بصرية فريدة للزوار.",
+        whatsapp: "966530559095"
+    },
+        {
+        id: "classic-w",
+        title: "مكتب انيق",
+        image: "imges/img/img16.jfif",
+        category: "Architecture",
+        tag: "Restaurant",
+        description: "دمج بين الهوية التقليدية واللمسات العصرية لتقديم تجربة بصرية فريدة للزوار.",
+        whatsapp: "966530559095"
+    },
+];
---
+++ js/data_interior.js
@@ -0,0 +1,26 @@
+const interiorData = [
+    {
+        id: "interior-1",
+        title: "تصميم فيلا مودرن",
+        image: "imges/img/img12.jfif",
+        category: "Interior Design",
+        description: "وصف مشروع الفيلا هنا بالتفصيل...",
+        whatsapp: "966530559095"
+    },
+    {
+        id: "interior-2",
+        title: "صالة استقبال فاخرة",
+        image: "imges/img/img13.jfif",
+        category: "Interior Design",
+        description: "وصف الصالة هنا بالتفصيل...",
+        whatsapp: "966530559095"
+    },
+        {
+        id: "interior-2",
+        title: "صالة استقبال فاخرة",
+        image: "imges/img/img5.jfif",
+        category: "Interior Design",
+        description: "وصف الصالة هنا بالتفصيل...",
+        whatsapp: "966530559095"
+    },
+];
