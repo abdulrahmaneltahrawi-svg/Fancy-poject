e583ec851ab3a69f3e4ec726cc5d18a9380ef3da
Abdulrahman <abdulrhmanelthrawi@gmail.com>
2026-05-12 08:52:36 +0300

    add all photo

--- js/data.js
+++ js/data.js
@@ -4,7 +4,6 @@ const projectsData = [
         id: "buffet",
         title: "aarkee contract",
         image: "imges/prodact/2 Large_67c9515e71832.webp",
-        category: "Interior Design",
         tag: "Restaurant",
         description: "ROMA PHONE BOTH",
         whatsapp: "966530559095"
@@ -13,7 +12,6 @@ const projectsData = [
         id: "office",
         title: "aarkee contract",
         image: "imges/prodact/AR-PC-TB-11 Large_67c965620e7ec.webp",
-        category: "Interior Design",
         tag: "Office",
         description: "PLANK",
         whatsapp: "966571778989"
@@ -22,7 +20,6 @@ const projectsData = [
         id: "classic-restaurant",
         title: "aarkee contract",
         image: "imges/prodact/AR-SHE-SO-03 Large_67c9581def87f.webp",
-        category: "Architecture",
         tag: "Restaurant",
         description: "SHEEP",
         whatsapp: "966530559095"
@@ -31,7 +28,6 @@ const projectsData = [
         id: "classic-sad",
         title: "Joys Living",
         image: "imges/prodact/CH-283A (5)_67ce822be6d7a.webp",
-        category: "Architecture",
         tag: "prodact",
         description: "TOFU",
         whatsapp: "966530559095"
@@ -40,7 +36,6 @@ const projectsData = [
         id: "classic-df",
         title: "aarkee office",
         image: "imges/prodact/CH-511黄棕拼 (2)_67ce93d7c2964.webp",
-        category: "Architecture",
         tag: "prodact",
         description: "PIVOT - LEATHER CHAIR",
         whatsapp: "966530559095"
@@ -49,7 +44,6 @@ const projectsData = [
         id: "classic-w",
         title: "aarkee office",
         image: "imges/prodact/JY-FXL-1-¥Û∫¸¿Í (3) Large 2_673ff62c50095.webp",
-        category: "Architecture",
         tag: "Restaurant",
         description: "SPIRE - MESH CHAIR",
         whatsapp: "966530559095"
--- js/data_interior.js
+++ js/data_interior.js
@@ -22,6 +22,30 @@ const interiorData = [
         category: "Interior Design",
         description: "وصف الصالة هنا بالتفصيل...",
         whatsapp: "966530559095"
+    },
+        {
+        id: "interior-1",
+        title: "تصميم فيلا مودرن",
+        image: "imges/magazine/cover_155.png",
+        category: "Interior Design",
+        description: "وصف مشروع الفيلا هنا بالتفصيل...",
+        whatsapp: "966530559095"
+    },
+    {
+        id: "interior-2",
+        title: "صالة استقبال فاخرة",
+        image: "imges/magazine/blob_68c9331e2777b..png",
+        category: "Interior Design",
+        description: "وصف الصالة هنا بالتفصيل...",
+        whatsapp: "966530559095"
+    },
+        {
+        id: "interior-3",
+        title: "صالة استقبال فاخرة",
+        image: "imges/magazine/blob_68c9306df0fdd..png",
+        category: "Interior Design",
+        description: "وصف الصالة هنا بالتفصيل...",
+        whatsapp: "966530559095"
     }
 
 ];
