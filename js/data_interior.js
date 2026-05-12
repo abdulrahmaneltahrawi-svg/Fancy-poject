f0a53903070ae0b70bcfbe3b9ebb71b306890f3c
Abdulrahman <abdulrhmanelthrawi@gmail.com>
2026-05-12 08:41:22 +0300

    magazine add

--- js/data_interior.js
+++ js/data_interior.js
@@ -2,7 +2,7 @@ const interiorData = [
     {
         id: "interior-1",
         title: "تصميم فيلا مودرن",
-        image: "imges/img/img12.jfif",
+        image: "imges/magazine/cover_152.png",
         category: "Interior Design",
         description: "وصف مشروع الفيلا هنا بالتفصيل...",
         whatsapp: "966530559095"
@@ -10,7 +10,7 @@ const interiorData = [
     {
         id: "interior-2",
         title: "صالة استقبال فاخرة",
-        image: "imges/img/img13.jfif",
+        image: "imges/magazine/cover_153.png",
         category: "Interior Design",
         description: "وصف الصالة هنا بالتفصيل...",
         whatsapp: "966530559095"
@@ -18,7 +18,7 @@ const interiorData = [
         {
         id: "interior-3",
         title: "صالة استقبال فاخرة",
-        image: "imges/img/img5.jfif",
+        image: "imges/magazine/cover_155.png",
         category: "Interior Design",
         description: "وصف الصالة هنا بالتفصيل...",
         whatsapp: "966530559095"
