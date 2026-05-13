56014c79917fc21cdfd86bf0e371fe8254c16504
Abdulrahman <abdulrhmanelthrawi@gmail.com>
2026-05-13 10:36:20 +0300

    edit prodact card

--- js/data.js
+++ js/data.js
@@ -4,46 +4,42 @@ const projectsData = [
         id: "buffet",
         title: "aarkee contract",
         image: "imges/prodact/2 Large_67c9515e71832.webp",
-        description: "ROMA PHONE BOTH",
+        category: "ROMA PHONE BOTH",
         whatsapp: "966530559095"
     },
     {
         id: "office",
         title: "aarkee contract",
         image: "imges/prodact/AR-PC-TB-11 Large_67c965620e7ec.webp",
-        description: "PLANK",
+        category: "PLANK",
         whatsapp: "966571778989"
     },
     {
         id: "classic-restaurant",
         title: "aarkee contract",
         image: "imges/prodact/AR-SHE-SO-03 Large_67c9581def87f.webp",
-    
-        description: "SHEEP",
+        category: "SHEEP",
         whatsapp: "966530559095"
     },
         {
         id: "classic-sad",
         title: "Joys Living",
         image: "imges/prodact/CH-283A (5)_67ce822be6d7a.webp",
-  
-        description: "SPIRE - MESH CHAIR",
+        category: "SPIRE - MESH CHAIR",
         whatsapp: "966530559095"
     },
         {
         id: "classic-df",
         title: "aarkee office",
         image: "imges/prodact/CH-511黄棕拼 (2)_67ce93d7c2964.webp",
-
-        description: "PIVOT - LEATHER CHAIR",
+        category: "PIVOT - LEATHER CHAIR",
         whatsapp: "966530559095"
     },
         {
         id: "classic-w",
         title: "aarkee office",
         image: "imges/prodact/JY-FXL-1-¥Û∫¸¿Í (3) Large 2_673ff62c50095.webp",
-   
-        description: "Fox Chair",
+        category: "Fox Chair",
         whatsapp: "966530559095"
     },
 ];
