9ef447b21f69b00f1a1aa346215fd52463547a48
Abdulrahman <abdulrhmanelthrawi@gmail.com>
2026-05-13 09:48:43 +0300

    finish magazine

--- js/data_interior.js
+++ js/data_interior.js
@@ -3,18 +3,27 @@ const interiorData = [
         id: "interior-1",
         title: "Îlot Balmoral Energizes Montreal’s Creative Economy as a New Addition to Downtown Skyline",
         image: "imges/magazine/cover_152.png",
-        category: "Interior Design",
-        description: "Architecuture interior design",
+        category: "Architecuture interior design",
         type: "office",
-        whatsapp: "966530559095",
         gallery: [
         "imges/magazine/1/64062-full_952-30_64062_sc_v2com_68ca1e1df25cd.webp",
         "imges/magazine/1/64075-full_952-30_64075_sc_v2com(1)_68ca1e21ebe76.webp",
         "imges/magazine/1/64070-full_952-30_64070_sc_v2com_68ca1e2094c68.webp",
         "imges/magazine/1/64067-full_952-30_64067_sc_v2com_68ca1e1f8ad75.webp",
         "imges/magazine/1/64066-full_952-30_64066_sc_v2com_68ca1e1f8be13.webp"
-    ]
+    ],
+    description: `
+        Montréal, Canada, 2022-04-05 - The Îlot Balmoral, a 13-storey mixed-use office building commissioned by the Société d’Habitation de Montréal (SHDM), rises as a testimony to Montréal’s creative economy. 
         
+        ### An architectural symbol of creativity
+        On the surface, Îlot Balmoral is a highly dynamic structure rising up from the urban fabric. The almost perfect cube is wrapped in exterior glass carefully selected for its white frit pattern and translucent panels. The glass façade provides an enormous infusion of light into the building, while the frit pattern serves to control thermal heat gain, meeting LEED Gold sustainability objectives.
+        
+        ### Anchoring the creative economy
+        The building serves as the new home of the National Film Board of Canada (NFB) and UQAC’s NAD School. A large internal atrium is carved diagonally through the centre of the building, delineated by a bright red external fold that contrasts against the clean glass exterior—an echo of the cultural branding of the Quartier des Spectacles.
+        
+        ### A beacon of vibrancy
+        The design strategy embraces the vibrancy of the district, drawing in pedestrian flow across the sunlit lobby. Pedestrian bridges on every level connect the building’s two volumes, providing spectacular views of the cityscape and Mont Royal.
+    `,    
     },
     {
         id: "interior-2",
