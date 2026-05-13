021041b332228ac4961a666a9a87b9151c59ab29
Abdulrahman <abdulrhmanelthrawi@gmail.com>
2026-05-13 09:57:20 +0300

    magazine page finish

--- js/data_interior.js
+++ js/data_interior.js
@@ -30,45 +30,95 @@ const interiorData = [
         title: "Restaurant Jérôme Ferrer by Europea",
         image: "imges/magazine/cover_153.png",
         category: "Interior Design",
-        description: "interior design",
         type: "Restaurant",
-        whatsapp: "966530559095"
+            description: `
+        Montréal, Canada, 2022-04-05 - The Îlot Balmoral, a 13-storey mixed-use office building commissioned by the Société d’Habitation de Montréal (SHDM), rises as a testimony to Montréal’s creative economy. 
+        
+        ### An architectural symbol of creativity
+        On the surface, Îlot Balmoral is a highly dynamic structure rising up from the urban fabric. The almost perfect cube is wrapped in exterior glass carefully selected for its white frit pattern and translucent panels. The glass façade provides an enormous infusion of light into the building, while the frit pattern serves to control thermal heat gain, meeting LEED Gold sustainability objectives.
+        
+        ### Anchoring the creative economy
+        The building serves as the new home of the National Film Board of Canada (NFB) and UQAC’s NAD School. A large internal atrium is carved diagonally through the centre of the building, delineated by a bright red external fold that contrasts against the clean glass exterior—an echo of the cultural branding of the Quartier des Spectacles.
+        
+        ### A beacon of vibrancy
+        The design strategy embraces the vibrancy of the district, drawing in pedestrian flow across the sunlit lobby. Pedestrian bridges on every level connect the building’s two volumes, providing spectacular views of the cityscape and Mont Royal.
+    `,    
     },
         {
         id: "interior-3",
         title: "New Spaces for Desjardins at the Montréal Tower",
         image: "imges/magazine/cover_155.png",
         category: "Interior Design",
-        description: "Architecuture interior design",
         type: "office",
-        whatsapp: "966530559095"
+            description: `
+        Montréal, Canada, 2022-04-05 - The Îlot Balmoral, a 13-storey mixed-use office building commissioned by the Société d’Habitation de Montréal (SHDM), rises as a testimony to Montréal’s creative economy. 
+        
+        ### An architectural symbol of creativity
+        On the surface, Îlot Balmoral is a highly dynamic structure rising up from the urban fabric. The almost perfect cube is wrapped in exterior glass carefully selected for its white frit pattern and translucent panels. The glass façade provides an enormous infusion of light into the building, while the frit pattern serves to control thermal heat gain, meeting LEED Gold sustainability objectives.
+        
+        ### Anchoring the creative economy
+        The building serves as the new home of the National Film Board of Canada (NFB) and UQAC’s NAD School. A large internal atrium is carved diagonally through the centre of the building, delineated by a bright red external fold that contrasts against the clean glass exterior—an echo of the cultural branding of the Quartier des Spectacles.
+        
+        ### A beacon of vibrancy
+        The design strategy embraces the vibrancy of the district, drawing in pedestrian flow across the sunlit lobby. Pedestrian bridges on every level connect the building’s two volumes, providing spectacular views of the cityscape and Mont Royal.
+    `,    
     },
         {
         id: "interior-7",
         title: "HEC Montréal’s Hélène Desmarais Building: A Contemporary Face for an Historic Institution",
         image: "imges/magazine/blob_68c93499e3d3c..png",
         category: "Interior Design",
-        description: "Architecuture interior design",
         type: "Schoole",
-        whatsapp: "966530559095"
+            description: `
+        Montréal, Canada, 2022-04-05 - The Îlot Balmoral, a 13-storey mixed-use office building commissioned by the Société d’Habitation de Montréal (SHDM), rises as a testimony to Montréal’s creative economy. 
+        
+        ### An architectural symbol of creativity
+        On the surface, Îlot Balmoral is a highly dynamic structure rising up from the urban fabric. The almost perfect cube is wrapped in exterior glass carefully selected for its white frit pattern and translucent panels. The glass façade provides an enormous infusion of light into the building, while the frit pattern serves to control thermal heat gain, meeting LEED Gold sustainability objectives.
+        
+        ### Anchoring the creative economy
+        The building serves as the new home of the National Film Board of Canada (NFB) and UQAC’s NAD School. A large internal atrium is carved diagonally through the centre of the building, delineated by a bright red external fold that contrasts against the clean glass exterior—an echo of the cultural branding of the Quartier des Spectacles.
+        
+        ### A beacon of vibrancy
+        The design strategy embraces the vibrancy of the district, drawing in pedestrian flow across the sunlit lobby. Pedestrian bridges on every level connect the building’s two volumes, providing spectacular views of the cityscape and Mont Royal.
+    `,    
     },
     {
         id: "interior-8",
         title: "Pôle Multimodal Vendôme Wins the International Architecture MasterPrize in the Transportation Category",
         image: "imges/magazine/blob_68c9331e2777b..png",
         category: "Interior Design",
-        description: "Architecuture Landscape",
         type: "office",
-        whatsapp: "966530559095"
+            description: `
+        Montréal, Canada, 2022-04-05 - The Îlot Balmoral, a 13-storey mixed-use office building commissioned by the Société d’Habitation de Montréal (SHDM), rises as a testimony to Montréal’s creative economy. 
+        
+        ### An architectural symbol of creativity
+        On the surface, Îlot Balmoral is a highly dynamic structure rising up from the urban fabric. The almost perfect cube is wrapped in exterior glass carefully selected for its white frit pattern and translucent panels. The glass façade provides an enormous infusion of light into the building, while the frit pattern serves to control thermal heat gain, meeting LEED Gold sustainability objectives.
+        
+        ### Anchoring the creative economy
+        The building serves as the new home of the National Film Board of Canada (NFB) and UQAC’s NAD School. A large internal atrium is carved diagonally through the centre of the building, delineated by a bright red external fold that contrasts against the clean glass exterior—an echo of the cultural branding of the Quartier des Spectacles.
+        
+        ### A beacon of vibrancy
+        The design strategy embraces the vibrancy of the district, drawing in pedestrian flow across the sunlit lobby. Pedestrian bridges on every level connect the building’s two volumes, providing spectacular views of the cityscape and Mont Royal.
+    `,    
     },
         {
         id: "interior-9",
         title: "Puro Hotel",
         image: "imges/magazine/blob_68c9306df0fdd..png",
         category: "Interior Design",
-        description: "interior design",
         type: "Hotel",
-        whatsapp: "966530559095"
+            description: `
+        Montréal, Canada, 2022-04-05 - The Îlot Balmoral, a 13-storey mixed-use office building commissioned by the Société d’Habitation de Montréal (SHDM), rises as a testimony to Montréal’s creative economy. 
+        
+        ### An architectural symbol of creativity
+        On the surface, Îlot Balmoral is a highly dynamic structure rising up from the urban fabric. The almost perfect cube is wrapped in exterior glass carefully selected for its white frit pattern and translucent panels. The glass façade provides an enormous infusion of light into the building, while the frit pattern serves to control thermal heat gain, meeting LEED Gold sustainability objectives.
+        
+        ### Anchoring the creative economy
+        The building serves as the new home of the National Film Board of Canada (NFB) and UQAC’s NAD School. A large internal atrium is carved diagonally through the centre of the building, delineated by a bright red external fold that contrasts against the clean glass exterior—an echo of the cultural branding of the Quartier des Spectacles.
+        
+        ### A beacon of vibrancy
+        The design strategy embraces the vibrancy of the district, drawing in pedestrian flow across the sunlit lobby. Pedestrian bridges on every level connect the building’s two volumes, providing spectacular views of the cityscape and Mont Royal.
+    `,    
     }
 
 ];
