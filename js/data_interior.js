c40d02b7012eb46fd01080dea8a51b20f6387b96
Abdulrahman <abdulrhmanelthrawi@gmail.com>
2026-05-13 08:47:18 +0300

    small edit

--- js/data_interior.js
+++ js/data_interior.js
@@ -35,7 +35,7 @@ const interiorData = [
         whatsapp: "966530559095"
     },
         {
-        id: "interior-1",
+        id: "interior-7",
         title: "HEC Montréal’s Hélène Desmarais Building: A Contemporary Face for an Historic Institution",
         image: "imges/magazine/blob_68c93499e3d3c..png",
         category: "Interior Design",
@@ -44,7 +44,7 @@ const interiorData = [
         whatsapp: "966530559095"
     },
     {
-        id: "interior-2",
+        id: "interior-8",
         title: "Pôle Multimodal Vendôme Wins the International Architecture MasterPrize in the Transportation Category",
         image: "imges/magazine/blob_68c9331e2777b..png",
         category: "Interior Design",
@@ -53,7 +53,7 @@ const interiorData = [
         whatsapp: "966530559095"
     },
         {
-        id: "interior-3",
+        id: "interior-9",
         title: "Puro Hotel",
         image: "imges/magazine/blob_68c9306df0fdd..png",
         category: "Interior Design",
